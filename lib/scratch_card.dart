c1fe49055a1831e6e076416bd1bfea6c0a14d44d
lalin <167679441+devlalin@users.noreply.github.com>
2025-01-26 17:55:52 +0530

    commit

--- lib/scratch_card.dart
+++ lib/scratch_card.dart
@@ -2,10 +2,6 @@
 import 'package:flutter/material.dart';
 import 'package:scratch_card/enum.dart';
 import 'package:scratch_card/scartch_card_src.dart';
-import 'dart:io';
-import 'package:flutter/material.dart';
-import 'dart:ui' as ui;
-import 'package:flutter/services.dart';
 export 'enum.dart';
 part 'scratch_card_widget.dart';
 
